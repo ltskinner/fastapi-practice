336473ca03f1e3a457194c6251e3a89447efcd60
ltskinner <33165286+ltskinner@users.noreply.github.com>
2023-04-14 11:37:44 -0500

    comment for cli exe

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ import uvicorn
 from webapp.application import app
 
 if __name__ == "__main__":
+    # uvicorn webapp.application:app --host 0.0.0.0 --port 8000
     uvicorn.run(app, host="0.0.0.0", port=8000)
