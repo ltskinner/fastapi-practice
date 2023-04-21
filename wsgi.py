d312f77120611ecd0d083b0b624c4249996eba3e
leete <33165286+ltskinner@users.noreply.github.com>
2023-04-21 01:53:34 -0500

    added application as obj in module

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import uvicorn
 
 from webapp.application import app
+from webapp.application import app as application
 
 if __name__ == "__main__":
     # uvicorn webapp.application:app --host 0.0.0.0 --port 8000
