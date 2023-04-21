5505a3e041715aa54373c962a3a0aa630171ca55
leete <33165286+ltskinner@users.noreply.github.com>
2023-04-21 02:06:32 -0500

    asgi wrapper

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 import uvicorn
 
 from webapp.application import app
-from webapp.application import app as application
+
+from a2wsgi import ASGIMiddleware
+
+application = ASGIMiddleware(app)
+
 
 if __name__ == "__main__":
     # uvicorn webapp.application:app --host 0.0.0.0 --port 8000
